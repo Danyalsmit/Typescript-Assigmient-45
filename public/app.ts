043d7d760b5b8356a49c5e97d1c02e85ed288851
Danyalsmit <dk818552@gmail.com>
2024-04-20 00:56:22 +0500

    97 assigment

---
+++ public/app.ts
@@ -0,0 +1,480 @@
+// 46
+// let laptop = {
+//   make: "dell",
+//   model: "Xps 15",
+//   year: 2021,
+//   describe: () => {
+//     console.log(`this laptop is a ${laptop.year}${laptop.make}${laptop.model}`);
+//   },
+// };
+// laptop.describe();
+
+import { log } from "console";
+
+// 47
+// let laptops = [
+//   { make: "dell", model: "XPS150", year: 2023 },
+//   { make: "Asus", model: "XPS50", year: 2020 },
+//   { make: "HP", model: "XPS15", year: 2021 },
+// ];
+// let [laptop1,laptop2,laptop3]= laptops;
+// console.log(laptop1);
+// console.log(laptop2);
+// console.log(laptop3);
+
+// 48
+// let laptopset1 = ["abc", "bda", "abc"];
+// let laptopset2 = ["aa", "aaa", "abf"];
+
+// let combinePrices = [...laptopset1, ...laptopset2].sort((a, b) => a.localeCompare(b));
+// console.log("combined",combinePrices);
+
+// 49
+// function logHobbies(...hobbies: string[]) {
+//   hobbies.forEach((hobby) => {
+//     console.log(`I enjoy ${hobby}`);
+//   });
+// }
+// logHobbies("reading", "coding", "ridingbike");
+
+// 50
+// let specialDay = `My ideal day would involve :
+// 1. waking up early and ging for a joging.
+// 2. spending a few hours coding on a personal project
+// 3. Ending a day by riding a bike`;
+// console.log(specialDay);
+
+// 51
+// function calculateArea(width: number, height: number) {
+//   return width * height;
+// }
+// const calculateArea = (width: number, height: number) => {
+
+//    let a = width * height;
+//    return a
+
+// }
+// console.log(calculateArea(5, 6));
+
+// 52
+// let smartphones = {
+//   make: "samsung",
+//   model: "Glaxy S10",
+//   spec: {
+//     storage: "212gb",
+//     screenSize: "6.2 inches",
+//   },
+// };
+// console.log(smartphones);
+
+// 53
+
+// let developerSkills = {
+//   language: ["javascript", "Typescript", "python"],
+//   Framework: ["React", "Angular", "Vue"],
+//   tools: ["Git", "WebPack", "Docker"],
+// };
+// let { language, Framework, tools } = developerSkills;
+// console.log(
+//   `languages${language[0]}.framework:${Framework[0]}, tools:${tools[2]}`
+// );
+// // 54
+// function createObject(key: string, value: string) {
+//   let dynamicObject :{[key:string]:string} ={};
+//   dynamicObject[key] = value;
+//   return dynamicObject;
+// }
+// let user = createObject("theme", "dark");
+// console.log(user);
+
+// 55
+// let numbers =[1,2,3,4,5]
+// let doubleNumbers = numbers.map((key,number)=>{
+//     return `Double${key} numbers${number * 2}`
+
+// })
+// console.log(`Double numbers${doubleNumbers}`);
+
+// 56
+// let mixedArray = [1, "apple", 2, "banana", true, "carrot"];
+// let stringsArray = mixedArray.filter(item => typeof item === "boolean");
+// console.log(stringsArray);
+
+// 57
+// let grades = [88, 94, 72, 99, 53, 77];
+// let averageGrade = grades.reduce((total, grade) => total + grade, 0) / grades.length;
+// console.log(averageGrade);
+
+// 58
+// function averageScore(...scores: number[]) {
+//   let total = scores.reduce((sum, score) => sum + score, 0);
+//  let  num = scores.length;
+//  console.log(num);
+
+//   return total / scores.length
+// }
+// console.log(averageScore(10,20,30,40));
+
+// 59
+// function makeAdder(valueToAdd:number):(number:number) => number{
+//   return function(number:number):number{
+//     return number + valueToAdd
+//   }
+// }
+// let addFive = makeAdder(7)
+// console.log(addFive(10));
+
+// 60
+// let userProfile =(function(){
+//     let name = "danyal";
+//     let age = 23;
+//     return{
+//         display:function(){
+//             console.log(`Name: ${name}, Age:${age}`);
+
+//         }
+//     }
+// })();
+// userProfile.display();
+
+// 61
+// enum vehicleType{
+//     car,
+//     truck,
+//     bike
+// }
+// console.log(vehicleType.bike);
+
+// 62
+// interface student {
+//     name:string;
+//     age:number;
+//     courses:string[];
+// }
+// let student : student = {
+//     name:"danyal",
+//     age:23,
+//     courses:["math","english"]
+
+// }
+// console.log(student);
+
+// // 63
+// type shape ={
+//     kind:"circle"| "Retangle";
+//     radius:number;
+//     width?:number;
+//     hight?:number;
+// }
+// let circle:shape ={
+//     kind:"circle",
+//     radius: 5
+// }
+// let retangle:shape ={
+//     kind:"Retangle",
+//     radius: 5,
+//     width:45,
+//     hight:25
+// }
+// console.log("circle",circle);
+// console.log("retangle",retangle);
+
+// // 64
+// function combineString(text:string,num:number){
+//     return text + num ;
+// }
+// console.log(combineString("Age:",85));
+
+// // 65
+// function remainder(num1: number, num2: number) {
+//     return num1 % num2;
+// }
+
+// console.log(remainder(4, 2));
+
+// 66
+// function checkBoth(val1: boolean, val2: boolean) {
+//   return val1 && val2;
+// }
+// console.log(checkBoth(true, true));
+
+// 67
+// function AddNumberToString(number1: number,numberString:string){
+//   return number1 + Number(numberString)
+// }
+// console.log(AddNumberToString(4,"5"));\
+
+// 68
+
+// function multipleDecimal(num1:number,num2:number){
+//   return Math.round((num1*num2 )*10)/100
+// }
+// console.log(multipleDecimal(1.4,2.4));
+
+// // 69
+// function divideAndRemeinder(divided:number,divisor:number){
+// let quotient = Math.floor(divided / divisor);
+
+// let reminder = divided % divisor
+
+// return {quotient,reminder}
+// }
+
+// console.log(divideAndRemeinder(13,4));
+
+//  70
+// function printNumberWithLet(){
+//   for (let i = 1; i<= 5;i++ ){
+//     console.log(i);
+//   }
+// }
+// printNumberWithLet()
+
+// // 71
+// let name = "ali";
+// name = "akhter"
+// console.log(name);
+
+// const age = "25";
+
+// try {
+//   age ="26"
+// } catch (error) {
+//   console.log("Cannot reassign a constant variable.");
+// }
+
+// 72
+// {
+//   let blockLet = "visible inside the block";
+//   let blockConst = "only inside the block";
+//   console.log(blockLet);
+//   console.log(blockConst);
+// }
+// try {
+//   console.log(blockLet);
+// } catch (error) {
+//   console.log("error 1");
+// }
+// try {
+//   console.log(blockConst);
+// } catch (error) {
+//   console.log("error 1");
+// }
+
+// 73
+// function updateVariable(){
+//   let number = 10;
+//   console.log("Initial value:",number);
+//   number = 20;
+//   console.log("update value :", number);
+// }
+// updateVariable()
+
+// 74
+
+// function swapNumber() {
+//   let a = 5,b = 10;
+//   console.log("a", a, "b=====", b);
+//   let temp = a;
+//     a = b;
+//     b = temp;
+//     console.log("After Swap : a =",a ,"b =",b);
+// }
+// swapNumber()
+
+// 75
+
+// function useCompoundOperators() {
+//   let x = 4;
+//   console.log("Initial x:", x);
+
+//   x += 2;
+//   console.log("After addition:", x); // Shows x after addition
+
+//   x -= 1; // Subtracts 1 from x
+//   console.log("After subtraction:", x); // Shows x after subtraction
+
+//   x *= 3; // Multiplies x by 3
+//   console.log("After multiplication:", x); // Shows x after multiplication
+
+//   x /= 2; // Divides x by 2
+//   console.log("After division:", x); // Shows x after division
+// }
+
+// useCompoundOperators();
+
+// // 76
+// function addNumber(num1:number,num2:number){
+
+//   return num1 + num2
+// }
+// console.log(addNumber(4,20));
+
+// // 77
+// function greatUser(name:string="anonymous"){
+//   console.log(`Hello ${name}`);
+
+// }
+// greatUser("kumail")
+// greatUser()
+
+// 78
+// function squareDecleration(num:number){
+//   return num * num
+// }
+
+// const squareExpresion = function(num:number){
+//   return num * num
+// }
+// console.log(squareDecleration(3));
+// console.log(squareExpresion(4));
+
+// 79
+// let car = {
+//   make: "toyota",
+//   model: "Corolla",
+//   year: 2000,
+// };
+// console.log(car.year);
+
+// 80
+// let car = {
+//   make: "toyota",
+//   model: "Corolla",
+//   year: 2000,
+// };
+// (car as any).color = "Blue"; // Using type assertion
+
+// car.year = 2005;
+// console.log(car);
+
+// 81
+// function logObjectProperties(obj: { [key: string]: any }) {
+//   for (let property in obj){
+//     console.log(`${obj[property]}`);
+
+//   }
+// }
+
+// const car: { [key: string]: any } = { make: "Toyota", model: "Corolla", year: 2021, color: "blue", extraFeature: true }; // Can have additional properties
+// logObjectProperties(car);
+
+// // 82
+// function stringLength(str:string){
+//   return str.length;
+// }
+// console.log(stringLength("mark"));
+
+// 83
+// function convertCase(str: string) {
+//   let upperStr = str.toUpperCase();
+//   let lowerStr = str.toLowerCase();
+
+//   console.log("Uppercase:", upperStr, "Lowercase:", lowerStr);
+// }
+
+// convertCase("Hello World");
+
+// // 84
+// function replaceJavascriptWithTypeScript(sentence:string){
+//   return sentence.replace(/javascript  /g ,"Typescript");
+// }
+// console.log(replaceJavascriptWithTypeScript("I love JavaScript and JavaScript is awesome!"));
+//
+// // 85
+// function findCodePosition(str:string){
+//   return str.indexOf("code")
+
+// }
+// console.log(findCodePosition("learnto code with javascript"));
+
+// 86
+
+// function hasJavascript(str:string){
+//   return str.includes("javascript")
+// }
+// console.log(hasJavascript("I love coding in javascript"));
+
+// 87
+// function ectractFirst10Char(str:string){
+//   return str.substring(0,10)
+// }
+//  console.log(ectractFirst10Char("I love coding in javascript"));
+
+// // 88
+// function roundToNearestInteger(num: number): number {
+//   return Math.round(num);
+// }
+
+// console.log(roundToNearestInteger(4.7));
+// console.log(roundToNearestInteger(4.4));
+
+// 89
+// function convertStringToNumber(str: string, num: number) {
+//   let stri = parseFloat(str);
+//   let nume = parseInt(num.toString());
+
+//   return `${stri},${nume}`;
+// }
+
+// console.log(convertStringToNumber("123.45", 6));
+
+// // 90
+// function isValueNan(str:any){
+//   return isNaN(str)
+// }
+// console.log(isValueNan("hello"));
+// console.log(isValueNan(1));
+
+// 91
+// let favoriteFruits:string[] =["Apple","Mango","Banana"];
+// favoriteFruits.push("grapes")
+//   console.log(favoriteFruits);
+
+// 92
+// function removeLastElement<T>(str:T[]):T| undefined{
+//   return str.pop()
+// }
+// const fruits: string[] = ["Apple", "Banana", "Cherry"];
+// console.log(removeLastElement(fruits));
+// console.log(fruits);
+
+// 93
+// function replaceBananaWithMango(fruits: string[]) {
+//   const index = fruits.indexOf("Banana");
+//   if (index !== -1) fruits[index] = "Mango";
+// }
+// const fruits: string[] = ["Apple", "Banana", "Cherry"];
+// replaceBananaWithMango(fruits);
+// console.log(fruits);
+
+// 94
+// const words: string[] = ["Hello", "World", "TypeScript", "JavaScript"];
+// const lengths: number[] = words.map(word => word.length);
+// console.log(lengths); 
+
+// // 95
+// function filterGreaterThanTen(numbers: number[]){
+//   return numbers.filter(number => number > 10);
+// }
+// const numbers: number[] = [5, 10, 15, 20, 25];
+// console.log(filterGreaterThanTen(numbers)); 
+
+// 96
+// function calculateSum(numbers: number[]): number {
+//   return numbers.reduce((accumulator, current) => accumulator + current, 0);
+// }
+
+// const numbers: number[] = [1, 2, 3, 4, 5];
+// console.log(calculateSum(numbers));
+
+97
+function getCurrentDateFormatted():string{
+const now  = new Date() ;
+const day = String(now.getDate()).padStart(2,"0");
+const month = String(now.getMonth() + 1);
+const year = now.getFullYear();
+return `${day}-${month}-${year}`;
+
+}
+console.log(getCurrentDateFormatted());
---
+++ public/index.ts
@@ -0,0 +1,418 @@
+//2 var name : string = "Danyal";
+// console.log(`Hello ${name} Would you learn to typescript today`);
+
+// import { Console, log } from "console";
+
+//3  var name: string = "Danyal";
+// console.log(name.toLowerCase() );
+// console.log( name.toUpperCase());
+// console.log( name.charAt(0).toLocaleUpperCase()+ name.slice(1).toLowerCase());
+
+// 4
+// console.log('Oscar Wild once said ,"Always forgive your enemies; noting annoys them so much."')
+
+// 5
+// let famousperson : string = "Oscar wild"
+// let message : string = `${famousperson} once said , "Always forgive your enemies;nothing annoys them so much."`
+// console.log(message)
+
+// 6
+// let person : string ="\t\n Danyal \t\n"
+// console.log(person)
+// console.log(person.trim()),
+
+// 7
+// console.log(10+5);//Addition
+// console.log(10-6);//subtraction
+// console.log(10/2);//division
+// console.log(10*3);//multiplication
+//8
+// console.log(4+4);//Addition
+// console.log(16-8);//subtraction
+// console.log(16/2);//division
+// console.log(2*4);//Multiplicatiom
+
+// 9
+// let favoriteNumber:number = 8;
+// console.log(`my favorite number is ${favoriteNumber}.`);
+
+// 10 danyal, 2024-03-06
+// This program prints a personal message.
+// let myName:string = "danyal"
+// console.log(`Hello${myName}would you like to learn some Typescript today`)
+
+// 11
+// let friends:string[]=["Ali","Dabeer","Azeem"];
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(friends[i])
+// }
+
+// 12
+// let names:string[]=["Alice","Bob","Mark"];
+// for(let name of names){
+// console.log(`hello ${name},would you learn some typescript today`)
+// }
+
+// 13
+// let transport :string[]=["honda","tesla","suzuki"];
+// transport.forEach(e => {
+//     console.log(`i would like to own,${e}`);
+// });
+
+// 14
+// let guest :string[]=['Ali','Mansoor','haris'];
+// guest.forEach(f=>{
+//     console.log(`Dear ${f} ,Would you like to join me for dinner?`)
+// })
+
+// 15
+// let guests: string[] = ["Albert","Einstein", "Marie Curie", "john"];
+// let notcomming = "Einstein";
+// console.log(`${notcomming} can't make it dinner.`);
+
+// let newOne = "Mark";
+// guests[guests.indexOf(notcomming)] = newOne ;
+// guests.forEach(f=>{
+//     console.log(`Dear${f} would you like to join me for dinner ?`);
+
+// })
+// 16
+// let guests:string[]=["john","Ali","Khan","haris"];
+// console.log(`Great news ! i found a bigger dinner table`);
+//  guests.unshift("newton");
+//  guests.splice(guests.length / 2 ,0,  "charles");
+// guests.push("imran");
+
+//  guests.forEach(f=>{console.log(`${f} hello Dear`)})
+
+// 17
+// let guests:string[]=["john","Ali","Khan","haris","Haseeb"];
+// console.log("unfortunately,I can only invite two guests");
+// while(guests.length > 2){
+// let removeGuests = guests.pop();
+// console.log(`sorry ${removeGuests} i can't invite you to dinner`);
+//
+// }
+// guests.forEach(f=>{
+// console.log(`Dear ${f} you're still invited to dinner .`);
+//
+// })
+// guests.splice(0,guests.length);
+// console.log(guests);
+
+// // 18
+// let places :string[]=["Australia","England","Pakistan","New zealand"];
+// console.log("same Order",places);
+// console.log("Alphabetic order",[...places].sort());
+// console.log("same order",places);
+// console.log("Reverse Alphabetic order",[...places].sort().reverse());
+// console.log("Orignal Order",places);
+// places.reverse();
+// console.log("reverse order",places)
+// places.sort();
+// console.log("Alphabetic order",places);
+// places.reverse();
+// console.log("Reverse Alphabetic order",places);
+
+// 19
+// let guests:string[]=["Albertr","Ali","Haris"];
+// console.log(`i an inviting ${guests.length} people to dinner.`);
+
+// 20
+// let cities:string[]=["Karachi","Lahore","Islamabad","Faislabad"];
+// console.log("Cities i like to visit",cities);
+
+// 21
+// let car:{name:string;model:string;year:number}={
+//     name:"honda",
+//     model:"old",
+//     year:1998,
+// };
+// console.log(`car info ${car.name}  and year ${car.year} `);
+
+// 22
+// let friend:string[]=["bob","mark","Alex"];
+// console.log(friend[3]);
+// friend[2];
+
+// 23
+// let name = "Azar";
+// console.log("name == 'Azar' ?i predict true");
+// console.log(name == "Azar");
+// console.log("name == fasil ? i  predict false");
+// console.log(name == "fasil");
+
+// // 24
+// // equality with string
+// console.log("testing equality withstring");
+// console.log("apple"== "apple");
+// // console.log('apple'=='Apple');
+// // using the lower  case function
+// console.log("Testing with lower case");
+// console.log("Apple".toLowerCase()=="apple");
+// // numeric test
+// console.log("Numeric tests");
+// console.log(10 > 5);
+// console.log(10 < 5);
+// // test using And or operator
+// console.log("test with and or");
+// console.log(true&&false);
+// console.log(true||false);
+// // test weather item is an array
+// let fruit:string[]=["Apple","Banana","Orange"]
+// console.log("Apple is an fruit?");
+// console.log(fruit.includes("Apple"));
+// // test item not in array
+// console.log("is mango not in fruits");
+// console.log(!fruit.includes("mango"));;
+
+// 25
+// let alien_color = "red";
+// if(alien_color == "red"){
+//     console.log("you just earned 5$ ");
+// }
+// if(alien_color == "green"){
+
+// }
+
+// // 26
+// let color = "green";
+// if(color == "red"){
+// console.log("you are fail");
+// }else{
+//   console.log( "you passed");
+// }
+// let color = "red";
+// if(color == "red"){
+// console.log("you will select correct");
+
+// }else{
+// console.log("you lost");
+
+// }
+
+// 27
+// let color = 'red';
+// if(color == "red"){
+//     console.log("red is correct");
+// }else if(color == "green"){
+// console.log("wrong color");
+// }else if(color == "yello"){
+// console.log("wrong selected");
+// }
+
+// let color2 = 'yellow';
+// if(color2 == "red"){
+//     console.log("red is wrong");
+// }else if(color2 == "green"){
+// console.log("wrong color");
+// }else if(color2 == "yellow"){
+// console.log("correct  selected");
+// }
+
+// let color1 = 'green';
+// if(color1 == "red"){
+//     console.log("wrong color");
+
+// }else if(color1 == "green"){
+//     console.log("green is correct");
+
+// }else if(color1 == "yello"){
+// console.log("wrong selected");
+// }
+
+// 28
+// let age: number = 25;
+// if (age < 2) {
+//   console.log("the person is a baby");
+// } else if (age < 13) {
+//   console.log("this person is a kid");
+// } else if (age < 20) {
+//   console.log("this person is a teenager");
+// } else if (age < 65) {
+//   console.log("this person is an adult");
+// } else {
+//   console.log("this person is an elder");
+// }
+
+// // 29
+// let favorite_fruits: string[] = ["Apple", "Mango", "Banana"];
+// if (favorite_fruits.includes("Apple")) {
+//   console.log("you really like Apple");
+// }
+// if (favorite_fruits.includes("Mango")) {
+//   console.log("you really like Mangos");
+// }
+
+// 30
+// let usernames: string[] = ["Admin", "user1", "user2", "user3", "user4"];
+// usernames.forEach((username) => {
+//   if (username === "Admin") {
+//     console.log("Hello Admin would you like to see a status report");
+//   } else {
+//     console.log(`HELLO ${username}, THnak you for logging in again.`);
+//   }
+// });
+
+// 31
+// let usernames: string[] = [];
+// if (usernames.length === 0) {
+//   console.log("we need to find some users!");
+// }
+
+// 32
+// let currentuser: string[] = ["user1", "user2", "user3"];
+// let newuser: string[] = ["admin", "user1", "user","uaua"];
+// newuser.forEach((new1) => {
+//   if (
+//     currentuser.some((current) => current.toLowerCase() === new1.toLowerCase())
+//   ) {
+//     console.log(`${new1} will need to enter new user`);
+//   } else {
+//     console.log(`${new1} is avaliable`);
+//   }
+// });
+
+// // 33
+// let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+// numbers.forEach(f=>{
+//   let suffix = "th"
+//   if(f === 1){
+//     suffix = "st";
+//   }else if(f === 2){
+//     suffix = "nd";
+//   }else if (f === 3){
+//     suffix = "rd";
+//   }
+//   console.log(`${f}${suffix}`);
+
+// })
+
+// 34
+// let pizzas: string[] = ["fajita", "pepperoni", "tikka"];
+// pizzas.forEach((pizza) => {
+//   console.log(`i like ${pizza}`);
+// });
+// console.log("i really love pizza");
+
+// 35
+// let animals: string[] = ["dog", "cat", "rabbit"];
+// animals.forEach((animal) => {
+//   console.log(`a ${animal} would make a great pet`);
+// });
+// console.log("Any of these animal would make a great pet");
+
+// 36(function)
+
+// function make_shirt(size: string, message: string) {
+//   console.log(
+//     `Making a ${size} t shirt with the message "${message}" printed on it`
+//   );
+// }
+// make_shirt("medium","you can do it!")
+
+// 37
+// function t_shirt(
+//   size: string = "large",
+//   message: string = "i love typescript"
+// ) {
+//   console.log(
+//     `making a ${size} shirt wiyh the message"${message} printed on it"`
+//   );
+// }
+// t_shirt()
+// t_shirt("medium")
+// t_shirt("medium","Dive in ty codding")
+
+// 38
+// function cities(city: string, country: string = "pakistan") {
+//   console.log(`${city} is in ${country} `);
+// }
+// cities("karachi");
+// cities("lahore");
+// cities("Tokyo","japan");
+
+// 39
+// function cityAndCountry(city: string, country: string): string {
+//   return `${city},${country}`;
+// }
+// console.log(cityAndCountry("lahore", "pakistan"));
+// console.log(cityAndCountry("tokyo", "japan"));
+
+// 40
+
+// function make_album(artist: string, title: string, tracks?: number) {
+//   let album = { artist, title,tracks };
+//   if (tracks) {
+//     album['tracks'] = tracks;
+//   }
+//   return album;
+// }
+// console.log(make_album("Artist One", "The First Album"));
+
+// // 41
+// // let megicians: string[] = ["alice", "bob", "rmark"];
+
+// function show_magicians(megicians:string[]){
+//   megicians.forEach(megician => {
+//     console.log(megician);
+
+//   });
+// };
+// show_magicians(megicians);
+
+// // 42
+// function make_great(magicians: string[]) {
+//   for (let i = 0; i < magicians.length; i++) {
+//       magicians[i] = magicians[i] + " the Great";
+//   }
+// }
+
+// make_great(megicians); // Modifies the original array
+// show_magicians(megicians);
+
+// 43
+// let megicians: string[] = ["Alice ", "Bob", "Mark", "David"];
+
+// function make_great(megicians: string[]): string[] {
+//   let greatMegicians :string []= [];
+//   megicians.forEach(megician => {
+//     greatMegicians.push(`${megician} the great`);
+//   });
+//   return greatMegicians;
+
+// }
+// function show_magicians(megicians:string[]){
+//     megicians.forEach(megician => {
+//       console.log(megician);
+
+//     });
+//   }
+// let greatMegicians = make_great(megicians); //modify array
+// console.log("orignal megicians");
+
+// show_magicians(megicians);
+// console.log("modify megicians");
+// show_magicians(greatMegicians);
+
+// // 44
+// function making_sandwich(...items: string[]) {
+//   console.log(`Making a sandwich with:${items.join(",")}.`);
+// }
+// making_sandwich("katchep", "chees");
+// making_sandwich("katchep", "chees", "tomato");
+// making_sandwich("katchep", "chees", "tomato","mustard","mayo");
+
+// 45
+// function make_car(
+//   manufacturer: string,
+//   model: string,
+//   ...options: [string, any][]
+// ): Object {
+//   let car :any = { manufacturer, model };
+//   options.forEach(([key, value]) => car[key] = value);
+//   return car;
+// }
+// console.log(make_car("Toyota", "Corolla",["color","red"],["year",2020]));
+// console.log(make_car("Ford", "Fiesta", ["color", "blue"], ["sunroof", true]));
+
